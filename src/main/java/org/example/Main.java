d5aef413bc4b9d3694d7fcfff3287acc56b1d8b9
pramodaya <pramodayajayalath@gmail.com>
2023-01-18 13:01:12 +0200

    Automation code added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,31 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "driver//chrome_driver//chromedriver");
+
+        // Create a new instance of the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the Google.com URL
+        driver.get("https://www.amazon.de/");
+
+        // Wait for 5 seconds to allow the user to see the page
+        Thread.sleep(5000);
+
+        // Wait for 5 seconds to allow the user to see the search results
+        Thread.sleep(5000);
+
+        // Close the browser
+        driver.quit();
     }
+
 }
